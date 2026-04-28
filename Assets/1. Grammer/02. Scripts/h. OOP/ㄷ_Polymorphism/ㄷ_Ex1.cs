a952650bcc8803618991fa34d1fe8ee190c0b734
Adonis <wintiger0908@gmail.com>
2026-04-28 16:46:57 +0900

    Update ㄷ_Ex1.cs

--- Assets/1. Grammer/02. Scripts/h. OOP/ㄷ_Polymorphism/ㄷ_Ex1.cs
+++ Assets/1. Grammer/02. Scripts/h. OOP/ㄷ_Polymorphism/ㄷ_Ex1.cs
@@ -40,7 +40,8 @@ public class ㄷ_Ex1 : MonoBehaviour
         Player player = new Player();
 
         // Monster 타입의 리스트에 Orc와 Ogre 객체가 Monster 타입으로 저장되어 있지만,
-        // 실제로는 Orc와 Ogre 객체가 저장되어 있기 때문에, Player 클래스의 Attack() 메서드를 호출할 때 각 객체의 OnDamaged() 메서드가 호출됩니다.
+        // 실제로는 Orc와 Ogre 객체가 저장되어 있기 때문에,
+        // Player 클래스의 Attack() 메서드를 호출할 때 각 객체의 OnDamaged() 메서드가 호출됩니다.
         foreach (Monster monster in monsters)
             player.Attack(monster); // Player 클래스의 Attack() 메서드를 호출하여 각 몬스터를 공격
     }
