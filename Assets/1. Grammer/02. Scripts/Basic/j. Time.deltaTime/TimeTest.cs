cf4255bf03ca8b30efbdea9df9c42737cb3792b4
Adonis <wintiger0908@gmail.com>
2026-05-31 20:14:48 +0900

    13. Time.deltaTime

--- Assets/1. Grammer/02. Scripts/Basic/j. Time.deltaTime/TimeTest.cs
+++ Assets/1. Grammer/02. Scripts/Basic/j. Time.deltaTime/TimeTest.cs
@@ -9,6 +9,39 @@ public class TimeTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x + Time.deltaTime, transform.position.y, transform.position.z);
+        float dt = Time.deltaTime;  // 프레임과 프레임 사이의 시간 간격을 구한다 (Update 함수가 호출되는 시간 간격)
+        transform.position = 
+        new Vector3(
+            transform.position.x, 
+            transform.position.y + dt * 1f, 
+            transform.position.z);
+    }
+
+    // 만약 1초에 10프레임의 속도를 가지고 있는 pc의 경우
+    // y축 10
+    // Time.deltaTime = 1초 / 10프레임 = 0.1초
+    // y축 10 * Time.deltaTime = 10 * 0.1초 = 1
+
+    // 만약 1초에 20프레임의 속도를 가지고 있는 pc의 경우
+    // y축 20
+    // Time.deltaTime = 1초 / 20프레임 = 0.05초
+    // y축 20 * Time.deltaTime = 20 * 0.05초 = 1
+
+    // 사용자의 pc 성능에 상관없이 동일한 퍼포먼스를 보여준다.
+
+    float time = 0f;
+
+    void Move1s()
+    {
+        time += Time.deltaTime;  // time에 Time.deltaTime을 더한다. (time이 1초가 될 때까지)
+        if (time >= 1f)  // time이 1초가 되면
+        {
+            transform.position = 
+            new Vector3(
+                transform.position.x, 
+                transform.position.y + 1f, 
+                transform.position.z);
+            time = 0f;  // time을 초기화한다.
+        }
     }
 }
