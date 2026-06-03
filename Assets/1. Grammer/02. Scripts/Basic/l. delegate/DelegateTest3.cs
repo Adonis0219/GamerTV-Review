3c060c3deda2bc5cf06a61bd5aa4d56d83943749
Adonis <wintiger0908@gmail.com>
2026-06-03 21:15:38 +0900

    delegate 복습 중간 저장

---
+++ Assets/1. Grammer/02. Scripts/Basic/l. delegate/DelegateTest3.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class DelegateTest3 : MonoBehaviour
+{
+    public delegate void TestDelegate();    // 델리게이트 타입 선언
+
+    TestDelegate _testDelegate;    // 델리게이트 변수 선언(객체 정의)
+
+    void Start()
+    {
+        _testDelegate = TargetF;   // 델리게이트 변수에 함수 대입
+
+        Do(_testDelegate);    // 델리게이트를 매개변수로 전달
+    }
+
+    void Do(TestDelegate del)   // 델리게이트를 매개변수로 받는 함수
+    {
+        del();  // 매개변수로 받은 델리게이트 호출
+        del.Invoke();   // 매개변수로 받은 델리게이트 호출
+    }
+
+    void TargetF()
+    {
+        // 여기 있는 코드 사용하고 싶음
+        Debug.Log("TargetF");
+    }
+}
