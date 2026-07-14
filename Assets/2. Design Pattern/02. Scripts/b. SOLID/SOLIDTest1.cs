31bece33d1a4446ef15efae00ddcc571f251b7df
Adonis <wintiger0908@gmail.com>
2026-07-14 16:58:28 +0900

    SOLID 중간 저장

---
+++ Assets/2. Design Pattern/02. Scripts/b. SOLID/SOLIDTest1.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class SOLIDTest1 : MonoBehaviour
+{
+    // SOLID
+    
+    // SRP(Single Responsibility Principle) : 단일 책임 원칙
+    // - 클래스, 모듈, 함수는 하나의 책임만 가져야 하며, 변경 이유도 하나여야 한다
+}
+    
